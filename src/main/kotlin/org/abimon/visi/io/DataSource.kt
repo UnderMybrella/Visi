9ab4a2c262bb617c211def11aca081de3ecead9d
Isaac <undermybrella@abimon.org>
2017-07-10 11:42:35 +1000

    Change the DataSources over to use getters rather than final variables for data and InputStream retrieval

--- src/main/kotlin/org/abimon/visi/io/DataSource.kt
+++ src/main/kotlin/org/abimon/visi/io/DataSource.kt
@@ -18,62 +18,66 @@ interface DataSource {
 }
 
 class FileDataSource(val file: File) : DataSource {
-    private var prev: FileInputStream? = null
 
     override val location: String = file.absolutePath
 
-    override val data: ByteArray = file.readBytes()
+    override val data: ByteArray
+        get() = file.readBytes()
 
-    override val inputStream: InputStream = run {
-        if (prev != null)
-            prev!!.close()
-        prev = FileInputStream(file)
-        return@run prev!!
-    }
+    override val inputStream: InputStream
+        get() = FileInputStream(file)
 
     override val size: Long = file.length()
 }
 
 class HTTPDataSource(val url: URL, val userAgent: String) : DataSource {
-
     constructor(url: URL) : this(url, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:44.0) Gecko/20100101 Firefox/44.0")
 
     override val location: String = url.toExternalForm()
 
-    override val data: ByteArray = run {
-        val baos = ByteArrayOutputStream()
-        use { it.writeTo(baos, 8192, true) }
-        return@run baos.toByteArray()
-    }
+    override val data: ByteArray
+        get() {
+            val baos = ByteArrayOutputStream()
+            use { it.writeTo(baos, 8192, true) }
+            return baos.toByteArray()
+        }
 
-    override val inputStream: InputStream = run {
-        val http = url.openConnection() as HttpURLConnection
-        http.requestMethod = "GET"
-        http.setRequestProperty("User-Agent", userAgent)
-        return@run if(http.responseCode < 400) http.inputStream else http.errorStream
-    }
+    override val inputStream: InputStream
+        get() {
+            val http = url.openConnection() as HttpURLConnection
+            http.requestMethod = "GET"
+            http.setRequestProperty("User-Agent", userAgent)
+            return if (http.responseCode < 400) http.inputStream else http.errorStream
+        }
 
-    override val size: Long = use { it.available().toLong() }
+    override val size: Long
+        get() = use { it.available().toLong() }
 }
 
 class FunctionalDataSource(val dataSupplier: Supplier<ByteArray>) : DataSource {
     override val location: String = "Supplier " + dataSupplier.toString()
 
-    override val data: ByteArray = dataSupplier.get()
+    override val data: ByteArray
+        get() = dataSupplier.get()
 
-    override val inputStream: InputStream = ByteArrayInputStream(data)
+    override val inputStream: InputStream
+        get() = ByteArrayInputStream(data)
 
-    override val size: Long = data.size.toLong()
+    override val size: Long
+        get() = data.size.toLong()
 }
 
 class FunctionDataSource(val dataFunc: () -> ByteArray): DataSource {
     override val location: String = dataFunc.toString()
 
-    override val data: ByteArray = dataFunc()
+    override val data: ByteArray
+        get() = dataFunc()
 
-    override val inputStream: InputStream = ByteArrayInputStream(dataFunc())
+    override val inputStream: InputStream
+        get() = ByteArrayInputStream(dataFunc())
 
-    override val size: Long = dataFunc().size.toLong()
+    override val size: Long
+        get() = dataFunc().size.toLong()
 
 }
 
@@ -81,10 +85,11 @@ class FunctionDataSource(val dataFunc: () -> ByteArray): DataSource {
 class InputStreamDataSource(val stream: InputStream) : DataSource {
     override val location: String = stream.toString()
 
-    override val data: ByteArray = stream.use { it.readBytes() }
+    override val data: ByteArray
+        get() = stream.use { it.readBytes() }
 
     override val inputStream: InputStream = stream
 
-    override val size: Long = stream.use { it.available().toLong() }
-
+    override val size: Long
+        get() = stream.use { it.available().toLong() }
 }
