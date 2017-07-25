43cec7d26301cf2ab91d177317f8a757de7a2082
Isaac <undermybrella@abimon.org>
2017-07-25 14:35:11 +1000

    Add SkeletonKey

---
+++ src/main/kotlin/org/abimon/visi/reflect/SkeletonKey.kt
@@ -0,0 +1,22 @@
+package org.abimon.visi.reflect
+
+import kotlin.reflect.KClass
+import kotlin.reflect.jvm.jvmName
+
+class SkeletonKey<T: Any>(val klass: KClass<out T>, val door: T?) {
+    constructor(door: T): this(door::class, door)
+    val properties = klass.java.declaredFields.map { field -> field.name to field.apply { isAccessible = true } }.toMap()
+    val methods = klass.java.declaredMethods.map { method -> method.apply { isAccessible = true} }.groupBy { method -> method.name }
+
+    operator fun get(variable: String): Any? = (properties[variable] ?: throw IllegalArgumentException("No property with the name $variable on $klass")).get(door)
+    operator fun invoke(name: String, vararg params: Any?): Any? {
+        val method = (methods[name] ?: throw IllegalArgumentException("No function with the name $name on $klass")).firstOrNull { method ->
+            method.parameterCount == params.size && method.parameterTypes.filterIndexed { index, parameter ->
+                if(params[index] == null)
+                    return@filterIndexed false
+                return@filterIndexed (params[index]!!::class.java != parameter && params[index]!!::class.javaPrimitiveType != parameter && params[index]!!::class.javaObjectType != parameter)
+            }.isEmpty() } ?: throw IllegalArgumentException("No function with the name $name and parameters ${params.map { (it ?: return@map "null")::class.jvmName }.joinToString()} on $klass")
+
+        return method(door, *params)
+    }
+}
