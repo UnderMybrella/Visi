2c99e16fff79ce7e3a0a3e24f880d2bc23575958
Isaac <undermybrella@abimon.org>
2017-07-29 17:44:41 +1000

    Fix VSecurityKtTest.kt

--- src/test/kotlin/org/abimon/visi/security/VSecurityKtTest.kt
+++ src/test/kotlin/org/abimon/visi/security/VSecurityKtTest.kt
@@ -1,14 +1,14 @@
 package org.abimon.visi.security
 
 import org.junit.Test
-import java.io.File
-import java.io.FileInputStream
+import java.io.ByteArrayInputStream
+import java.security.SecureRandom
 import kotlin.system.measureTimeMillis
 
 class VSecurityKtTest {
     @Test
     fun fileTest() {
-        val ms = measureTimeMillis { println("Hash: " + FileInputStream(File("test.png")).sha256Hash()) }
+        val ms = measureTimeMillis { println("Hash: " + SecureRandom().run { val bytes = ByteArray(8192); nextBytes(bytes); return@run ByteArrayInputStream(bytes) }.sha256Hash()) }
         println("Took $ms ms")
     }
 }
